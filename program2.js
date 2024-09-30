4bba874ea33926525afb22cdd5419f29e3af2962
Akshat Singh <110585273+Singhakshat14@users.noreply.github.com>
2024-09-30 14:14:20 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -3,5 +3,12 @@
  * @return {number}
  */
 var romanToInt = function(s){
-    const 
+    const  romanValues = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
 }
